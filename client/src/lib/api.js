4139c12f34d49709cae8d9220c2c94b523f2141c
Rakesh <rakeshdarda19@gmail.com>
2026-01-21 17:22:21 +0530

    add changes in api

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 15000,
+  timeout: 0,
   headers: {
     "Content-Type": "application/json",
   },
